701f81b4e9b5df542d3675fb6c0cc0ba5bdf43c9
hanli <hkl450513@163.com>
2018-03-29 16:06:36 +0800

    add(styles)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AccountComponent } from './account/account.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
