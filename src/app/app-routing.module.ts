db59cb29201cfef742805bdbc8b30dc3c62656d7
hanli <hkl450513@163.com>
2018-03-29 18:22:06 +0800

    fixed(style)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
-  {path:"",children:[]}
+  // {path:"",children:[]},
+  {path:"", component: HomeComponent},
+  {path:"login", component: LoginComponent},
+  {path:"account", component: AccountComponent}
 ];
 
 @NgModule({
