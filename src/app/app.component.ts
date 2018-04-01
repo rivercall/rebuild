3a0c661b44fd6b4b090646e2be0cc54f99fd8d0f
hanli <hkl450513@163.com>
2018-04-01 19:28:52 +0800

    add(vertification)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,8 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  constructor (private router:Router ){
-
+  constructor (private router:Router){
+    
   }
 
   // header组件默认显示
@@ -18,11 +18,14 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.router.events.subscribe((event) => {
+      // console.log(event)
       if (event instanceof NavigationEnd) {     // 当导航成功结束时执行
         if(event.url=="/home"||event.url=="/account"){
           this.isHeaderShow =false;
+          this.isBackground =true;
         }
         if(event.url=="/login"){
+          this.isHeaderShow = true;
           this.isBackground = false;
         }
       }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,25 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  private getVertification: string = "获取验证码";
-  private second:number = 60;
-  private timer;
+  private getVertification: string = "获取验证码"; //验证码显示文字
+  private second:number = 5; //剩余时间
+  private timer; //定时器
+  private isSending=false; //是否正在发送验证码
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit() {
-    
+
   }
-  nowTest(){
+
+  // 更新验证码按钮样式
+  getVertif(){
+    if(this.isSending){
+      return;
+    }
+    this.isSending = true;
     this.timer = setInterval(() => {
       if(this.second<=0){
         this.getVertification ="获取验证码";
-        this.second = 60;
+        this.second = 5;
+        this.isSending = false;
         clearInterval(this.timer);
       }else{
-        this.getVertification = this.second+"秒";
+        this.getVertification = this.second+"s";
         this.second --; 
+        this.isSending =true;
       }
     }, 1000);
+  }
+
+  // 登录
+  login(){
+    this.route.navigate(['/home'])
   }
 }
